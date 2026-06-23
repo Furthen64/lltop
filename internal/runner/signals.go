819afe761c1499102029ec2405af61219576037e
Jörgen Engström <jorgen.engstrom@uht.se>
2026-06-23 11:32:47 +0200

    fixes for windows to sigkill

--- internal/runner/signals.go
+++ internal/runner/signals.go
@@ -1,11 +1,18 @@
 package runner
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"syscall"
 )
 
 func sendInterrupt(process *os.Process) error {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("taskkill", "/PID", fmt.Sprintf("%d", process.Pid))
+		return cmd.Run()
+	}
 	return process.Signal(syscall.SIGINT)
 }
 
